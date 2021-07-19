c6bb905f4751fe95c41af2facb607dd6462b8846
Miguel Rodriguez <mrodriguez1232@gmail.com>
2021-07-19 16:09:08 -0400

    Functions Labs

--- labs/06_functions/06_02_stats.py
+++ labs/06_functions/06_02_stats.py
@@ -6,8 +6,23 @@ Print these values to the console when calling the function.
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
-  # define the function here
-  pass
+def stats(num_list):
+
+    sum = 0
+    for n in num_list:
+      sum += n
+
+    max_num = max(num_list)
+    print(f"Max Number: {max_num}")
+
+    min_num = min(num_list)
+    print(f"Min Number: {min_num}")
+
+    average = float(sum / len(num_list))
+    print(f"Average: {average}")
+
+    print(f"Sum: {sum}")
 
 # call the function below here
+print(example_list)
+stats(example_list)
